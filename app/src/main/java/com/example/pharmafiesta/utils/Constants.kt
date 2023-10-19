4e6fc4f7ca84a359f8dfc68183468e41643e424a
Heba Elsaid <72816466+hebaelsaid912@users.noreply.github.com>
2023-10-19 17:56:41 +0300

    - handle sign in screen ui - add navigation from sign up screen - add google and facebook icons

--- app/src/main/java/com/example/pharmafiesta/utils/Constants.kt
+++ app/src/main/java/com/example/pharmafiesta/utils/Constants.kt
@@ -3,4 +3,5 @@ package com.example.pharmafiesta.utils
 object Constants {
     const val SPLASH_SCREEN_ROUTE : String = "splash_screen"
     const val SIGNUP_SCREEN_ROUTE : String = "signup_screen"
+    const val SIGNIN_SCREEN_ROUTE : String = "signin_screen"
 }
