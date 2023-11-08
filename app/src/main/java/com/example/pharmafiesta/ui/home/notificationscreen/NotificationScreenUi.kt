6dc29c5d8c14a165cfeaff9078e4495e04d8c1f9
Heba Elsaid <72816466+hebaelsaid912@users.noreply.github.com>
2023-11-08 09:47:19 +0200

    - handle basic text fot bills, chat and notification

--- app/src/main/java/com/example/pharmafiesta/ui/home/notificationscreen/NotificationScreenUi.kt
+++ app/src/main/java/com/example/pharmafiesta/ui/home/notificationscreen/NotificationScreenUi.kt
@@ -1,11 +1,27 @@
 package com.example.pharmafiesta.ui.home.notificationscreen
 
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import com.example.pharmafiesta.R
+import com.example.pharmafiesta.ui.theme.Black
 
 private const val TAG = "NotificationScreenUi"
 
 @Composable
 fun NotificationScreenUi () {
-
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(id = R.string.bottomNav_notification_title), fontSize = 24.sp, color = Black)
+    }
 }
